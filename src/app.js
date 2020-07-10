6c62864bd93d9a570c2b91dd666dae6cf950abfa
Rafael Athayde de Paula <raffaeo@gmail.com>
2020-07-10 03:15:32 -0300

    Desafio Ok

---
+++ src/app.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid, isUuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+function validateProjectId(request, response, next){
+  const { id } = request.params
+  if(!isUuid(id)){
+    return response.status(400).json({error: 'Inavalid project ID'})
+  }
+
+  return next();
+}
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+
+  return response.json(repositories)
+
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+  
+  repositories.push(repository)
+  return response.json(repository)
+});
+
+app.put("/repositories/:id", validateProjectId, (request, response) => {
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({error: "Repository not Found"})
+  }
+
+    const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
+    }
+  
+  repositories[repositoryIndex] = repository
+  return response.status(200).json(repository)
+});
+
+app.delete("/repositories/:id", validateProjectId, (request, response) => {
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id )
+
+  repositories.splice(repositoryIndex, 1)
+  return response.status(204).send()
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params
+
+  const repository = repositories.find(repository => repository.id === id)
+
+  if(!repository){
+    return response.status(400).send()
+  }
+  repository.likes += 1
+
+  return response.json(repository)
+  
+});
+
+module.exports = app;
